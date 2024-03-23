ccaf983a98636f3be9bc1e41d15ac46332f8aaee
André Andrade <andrecandrad@gmail.com>
2024-03-23 20:27:52 -0300

    instalação do echarts, echartsjs e removendo arquivos css padrao

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
 import Button from "@mui/material/Button";
+import Chart from "./components/Chart";
 
 function App() {
   return (
     <>
       <Button variant="outlined">Hello world</Button>
+      <Chart/>
     </>
   )
 }
