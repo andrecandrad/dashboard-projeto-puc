b3f29795fa72b873b307778e5c625adc1f85271c
Andre Andrade <andrecandrad@gmail.com>
2024-04-22 18:54:31 -0300

    commit do template

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,13 @@
 import "./App.css";
 import Button from "@mui/material/Button";
 import Chart from "./components/Chart";
+import MainGrid from "./components/MainGrid";
 
 function App() {
   return (
-    <>
-      <Button variant="outlined">Hello world</Button>
-      <Chart />
-      <h1 className="text-3xl font-bold underline text-red-500">Hello world!</h1>
-    </>
+    <div className="w-screen h-screen">
+      <MainGrid></MainGrid>
+    </div>
   );
 }
 
---
+++ src/components/MainGrid.jsx
@@ -0,0 +1,18 @@
+const MainGrid = () => {
+    return <div className="grid grid-cols-3 grid-rows-5 gap-14 bg-slate-200 w-full h-full p-14">
+        <div className="border-2 border-gray-300 rounded-sm shadow-lg col-span-1 row-span-3">
+            a
+        </div>
+        <div className="border-2 border-gray-300 rounded-sm shadow-lg col-span-1 row-span-3">
+            a
+        </div>
+        <div className="border-2 border-gray-300 rounded-sm shadow-lg col-span-1 row-span-5">
+            as
+        </div>
+        <div className="border-2 border-gray-300 rounded-sm shadow-lg col-span-2 row-span-2">
+            a
+        </div>
+    </div>
+}
+
+export default MainGrid;
