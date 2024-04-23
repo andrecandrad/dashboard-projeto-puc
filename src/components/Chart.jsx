8eb036dee2861c4b084c9b165553c67d9c6b5ad9
Gabriel Amorim <mrgconta@gmail.com>
2024-04-22 22:32:52 -0300

    teste

--- src/components/Chart.jsx
+++ src/components/Chart.jsx
@@ -1,50 +1,24 @@
 import EChartsReact from "echarts-for-react";
 
 const Chart = () => {
+
     const option = {
-        tooltip: {
-          trigger: 'item'
-        },
-        legend: {
-          top: '5%',
-          left: 'left',
-          orient: 'vertical'
-        },
-        series: [
-          {
-            name: 'Access From',
-            type: 'pie',
-            radius: ['40%', '70%'],
-            avoidLabelOverlap: false,
-            itemStyle: {
-              borderRadius: 10,
-              borderColor: '#fff',
-              borderWidth: 2
-            },
-            label: {
-              show: false,
-              position: 'center'
-            },
-            emphasis: {
-              label: {
-                show: true,
-                fontSize: 12,
-                fontWeight: 'bold'
-              }
-            },
-            labelLine: {
-              show: false
-            },
-            data: [
-              { value: 1048, name: 'Search Engine' },
-              { value: 735, name: 'Direct' },
-              { value: 580, name: 'Email' },
-              { value: 484, name: 'Union Ads' },
-              { value: 300, name: 'Video Ads' }
-            ]
+      yAxis: {
+        data: [],
+      },
+      series: [
+        {
+          type: 'bar',
+          data: [100, 200, 500, 600, ],
+          label: {
+            show: true,
+            position: 'right',
+            valueAnimation: true
           }
-        ]
-      };
+        }
+      ],
+    };
+    
       
     return <div className="teste"><EChartsReact option={option}/></div>
 }
--- src/components/MainGrid.jsx
+++ src/components/MainGrid.jsx
@@ -1,13 +1,16 @@
 import { RiMoneyDollarCircleFill } from "react-icons/ri";
 import { RiMoneyDollarCircleLine } from "react-icons/ri";
 import { FaCoins } from "react-icons/fa";
+import Chart from "./Chart";
+import ChartPie from "./ChartPie";
 
 const MainGrid = () => {
-  const colors = [];
+  // const colors = [];
   const valores = [158661.0, 14064.0, 36837.59];
-  const maxValue = Math.max(...valores);
+  // const maxValue = Math.max(...valores);
 
   return (
+
     <div className="grid grid-cols-3 grid-rows-5 gap-14 bg-slate-200 w-full h-full p-14">
       <div className="rounded-sm shadow-lg col-span-1 row-span-3 p-5">
         <div className="bg-gray-300 py-7 rounded-xl flex flex-col gap-3">
@@ -20,25 +23,15 @@ const MainGrid = () => {
             <h3 className="text-2xl text-green-600 font-bold">R$ 1.697.602,55</h3>
           </div>
         </div>
-
-        {/* novo componente */}
         <div className="flex flex-col text-lg gap-2 mt-7">
-          {/* <div className="bg-green-600/60 flex justify-between px-4 py-1 rounded-full min-2 w-11/12">
-            <p className="font-semibold">Valor Máximo</p>{" "}
-            <p className="text-green-800 font-semibold">{`R$ ${valores[0]}`}</p>
-          </div>
-
-          <div className="bg-green-600/20 flex justify-between px-4 py-1 rounded-full min-2 w-6/12">
-            <p className="font-semibold">Valor Mínimo</p>{" "}
-            <p className="text-green-800 font-semibold">{`R$ ${valores[1]}`}</p>
-          </div> */}
-
           <div className="bg-green-600/40 flex justify-between px-4 py-1 rounded-full min-2 w-8/12">
             <p className="font-semibold">Valor Médio</p>{" "}
             <p className="text-green-800 font-semibold">{`R$ ${valores[2]}`}</p>
           </div>
+        <Chart/>
         </div>
       </div>
+
       <div className="rounded-sm shadow-lg col-span-1 row-span-3 p-5">
         <div className="bg-gray-300 py-7 rounded-xl flex flex-col gap-3">
           <div className="flex justify-center items-center gap-5">
@@ -50,8 +43,6 @@ const MainGrid = () => {
             <h3 className="text-2xl text-red-600 font-bold">R$ 1.828.049,67</h3>
           </div>
         </div>
-
-        {/* novo componente */}
         <div className="flex flex-col text-lg gap-2 mt-7">
           <div className="bg-red-600/60 flex justify-between px-4 py-1 rounded-full min-2 w-11/12">
             <p className="font-semibold">Valor Máximo</p>{" "}
@@ -68,7 +59,9 @@ const MainGrid = () => {
             <p className="text-red-800 font-semibold">{`R$ ${valores[2]}`}</p>
           </div>
         </div>
+        <ChartPie/>
       </div>
+
       <div className="rounded-sm shadow-lg col-span-1 row-span-5 p-5">
         <div className="bg-gray-300 py-7 rounded-xl flex flex-col gap-3">
           <div className="flex justify-center items-center gap-5">
@@ -78,10 +71,9 @@ const MainGrid = () => {
           <div className="flex justify-center items-center gap-5">
             <h4 className="text-xl font-semibold">Valor médio no período:</h4>
             <h3 className="text-2xl text-cyan-600 font-bold">R$ 50.302,07</h3>
-          </div>
+          </div>          
         </div>
 
-        {/* novo componente */}
         <div className="flex flex-col text-lg gap-2 mt-7">
           <div className="bg-cyan-600/60 flex justify-between px-4 py-1 rounded-full min-2 w-11/12">
             <p className="font-semibold">Valor Máximo</p>{" "}
@@ -92,15 +84,10 @@ const MainGrid = () => {
             <p className="font-semibold">Valor Mínimo</p>{" "}
             <p className="text-cyan-800 font-semibold">{`R$ ${valores[1]}`}</p>
           </div>
-{/* 
-          <div className="bg-cyan-600/40 flex justify-between px-4 py-1 rounded-full min-2 w-8/12">
-            <p className="font-semibold">Valor Médio</p>{" "}
-            <p className="text-cyan-800 font-semibold">{`R$ ${valores[2]}`}</p>
-          </div> */}
         </div>
       </div>
+
       <div className="border-2 border-gray-300 rounded-sm shadow-lg col-span-2 row-span-2">
-        a
       </div>
     </div>
   );
